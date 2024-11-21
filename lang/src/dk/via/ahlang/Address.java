5b8312f9e7cb9e118bf108b5cfce06acb23af78d
ForgottenIce <38312721+ForgottenIce@users.noreply.github.com>
2024-11-21 16:10:23 +0100

    Create Address.java
    Co-Authored-By: Simon S.C <95408442+Lemske@users.noreply.github.com>
    Co-Authored-By: bobbobogaming <42958005+bobbobogaming@users.noreply.github.com>

---
+++ lang/src/dk/via/ahlang/Address.java
@@ -0,0 +1,41 @@
+package dk.via.ahlang;
+
+public class Address
+{
+    public int level;
+    public int displacement;
+
+
+    public Address()
+    {
+        level = 0;
+        displacement = 0;
+    }
+
+
+    public Address(int level, int displacement)
+    {
+        this.level = level;
+        this.displacement = displacement;
+    }
+
+
+    public Address(Address a, int increment)
+    {
+        this.level = a.level;
+        this.displacement = a.displacement + increment;
+    }
+
+
+    public Address(Address a)
+    {
+        this.level = a.level + 1;
+        this.displacement = 0;
+    }
+
+
+    public String toString()
+    {
+        return "level=" + level + " displacement=" + displacement;
+    }
+}
